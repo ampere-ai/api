a52333218c3c52bb1ed93d1b74b80979d18cd8bf
f1nniboy <me@f1nn.space>
2023-08-25 14:29:01 +0200

    Calculate cost for GPT models

--- src/models/text/gpt.ts
+++ src/models/text/gpt.ts
@@ -1,9 +1,29 @@
 import { fetchEventSource } from "@waylaidwanderer/fetch-event-source";
 
 import { OpenAIMessage } from "../../types/message.js";
-import { createModel } from "../mod.js";
 import { OPENAI_API_KEY } from "../../config.js";
 import { APIError } from "../../types/error.js";
+import { createModel } from "../mod.js";
+import { getChatMessageLength, getMessageTokens } from "../../utils/tokens.js";
+
+type OpenAIModel = "gpt-3.5-turbo" | "gpt-4" | "gpt-3.5-turbo-16k"
+
+const prices = {
+	"gpt-3.5-turbo": {
+		input: 0.0015,
+		output: 0.002
+	},
+
+	"gpt-4": {
+		input: 0.03,
+		output: 0.06
+	},
+
+	"gpt-3.5-turbo-16k": {
+		input: 0.003,
+		output: 0.004
+	}
+};
 
 export default createModel({
 	name: "gpt",
@@ -31,12 +51,16 @@ export default createModel({
 	},
 
 	execute: async ({ messages, maxTokens, model, temperature }, emitter) => {
-		const result: any = {
+		const result = {
 			content: "",
 			finishReason: null,
 			done: false,
 			cost: 0
 		};
+
+		result.cost += (
+			getChatMessageLength(...messages) / 1000
+		) * prices[model as OpenAIModel].input;
 
 		await fetchEventSource("https://api.openai.com/v1/chat/completions", {
 			method: "POST",
@@ -67,6 +91,11 @@ export default createModel({
 			onmessage: ({ data: raw }) => {
 				if (raw === "[DONE]") {
 					result.done = true;
+
+					result.cost += (
+						getMessageTokens(result.content) / 1000
+					) * prices[model as OpenAIModel].output;
+					
 					return emitter.emit(result);
 				}
 
@@ -74,6 +103,8 @@ export default createModel({
 				if (!data.choices || !data.choices[0] || !data.choices[0].delta.content) return;
 
 				result.content += data.choices[0].delta.content;
+				result.finishReason = data.choices[0].finish_reason ?? null;
+
 				emitter.emit(result);
 			}
 		});
---
+++ src/utils/tokens.ts
@@ -0,0 +1,28 @@
+import { get_encoding } from "@dqbd/tiktoken";
+const encoder = get_encoding("cl100k_base");
+
+import type { OpenAIMessage } from "../types/message.js";
+
+/** Count together all tokens contained in a list of conversation messages. */
+export function getChatMessageLength(...messages: OpenAIMessage[]) {
+	/* Total tokens used for the messages */
+	let total: number = 0;
+
+	for (const message of messages) {
+		/* Map each property of the message to the number of tokens it contains. */
+		const propertyTokenCounts = Object.values(message).map(value => {
+			/* Count the number of tokens in the property value. */
+			return getMessageTokens(value);
+		});
+
+		/* Sum the number of tokens in all properties and add 4 for metadata. */
+		total += propertyTokenCounts.reduce((a, b) => a + b, 4);
+	}
+
+	return total + 2;
+}
+
+/** Count together all the tokens in a string. */
+export function getMessageTokens(content: string) {
+	return encoder.encode(content).length;
+}
