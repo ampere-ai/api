28e3449e383dea9dba9157faad66dd11489f5c41
f1nniboy <me@f1nn.space>
2023-11-07 20:12:52 +0100

    Fix

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -6,11 +6,11 @@ import type { OpenAIMessage } from "../types/chat.js";
 /** Count together all tokens contained in a list of conversation messages. */
 export function getChatMessageLength(...messages: OpenAIMessage[]) {
 	/* Total tokens used for the messages */
-	let total: number = 0;
+	let total = 0;
 
 	for (const message of messages) {
 		/* Map each property of the message to the number of tokens it contains. */
-		const propertyTokenCounts = Object.values(message).map(value => {
+		const propertyTokenCounts = Object.values(message).filter(value => typeof value === "string").map(value => {
 			/* Count the number of tokens in the property value. */
 			return getMessageTokens(value);
 		});
